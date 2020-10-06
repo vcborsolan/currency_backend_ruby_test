17f171112d0271a57db9fc1a461df05cc080a857
Victor Orsolan <vorsolan@hotmail.com>
2020-10-06 12:23:19 -0300

    some progress in convert direct and same currency

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,8 +1,21 @@
 class Currency
 
     def initialize
-        @current_currency = {
-
+        @current_currencys = {
+            USD: {BRL: 5.65},
+            ARS: {BRL: 0.07},
+            EUR: {USD: 1.18},
+            GBP: {BRL: 7.24},
+            BTC: {USD: 10700.0}
         }
+    end
+
+    def convert(value , current_currency , target_currency)
+        if current_currency == target_currency
+            return true
+        elsif @current_currencys[current_currency].keys.first == target_currency
+            return value*@current_currencys[current_currency].values.first
+        # elsif 
+        end
     end
 end
--- spec/currency_spec.rb
+++ spec/currency_spec.rb
@@ -3,11 +3,17 @@ require 'currency'
 
 describe 'Currency converter' do
     it 'Deve verificar se a moeda atual é igual a moeda objetivo'  do
-        # currency = Currency.new
-        # expect(soma(1, 3)).to eq(4)
+        currency = Currency.new
+        expect(currency.convert(1.0 , :BRL , :BRL)).to eq(true)
     end
     it 'Caso tenha conversor direto , o realizar corretamente' do
-        
+        currency = Currency.new
+        expect(currency.convert(1.0 , :USD , :BRL)).to eq(5.65)
+        expect(currency.convert(1.0 , :ARS , :BRL)).to eq(0.07)
+        expect(currency.convert(1.0 , :EUR , :USD)).to eq(1.18)
+        expect(currency.convert(1.0 , :GBP , :BRL)).to eq(7.24)
+        expect(currency.convert(1.0 , :BTC , :USD)).to eq(10700.0)
+
     end
     it 'Caso nao tenha conversor direto , o relizar corretamente' do
         
