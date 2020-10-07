627eb89a5cb832b0bf68807888dd51c104e0207f
Victor Orsolan <vorsolan@hotmail.com>
2020-10-07 12:22:57 -0300

    some advance in converting non direct convert

--- lib/currency.rb
+++ lib/currency.rb
@@ -9,13 +9,29 @@ class Currency
             BTC: {USD: 10700.0}
         }
     end
-
+        #       1.0    |     EUR          |      GBP   
     def convert(value , current_currency , target_currency)
         if current_currency == target_currency
-            return true
-        elsif @current_currencys[current_currency].keys.first == target_currency
-            return value*@current_currencys[current_currency].values.first
-        # elsif 
+            return value
+        elsif @current_currencys.key?(current_currency) && @current_currencys[current_currency].key?(target_currency)
+            return (value*@current_currencys[current_currency].values.first).round(2)
+
+            # =======================================================================
+
+        elsif @current_currencys[current_currency]
+            value = value*@current_currencys[current_currency].values.first
+            current_currency = @current_currencys[current_currency].keys.first
+            puts current_currency
+            value = convert(value , current_currency , target_currency)
+
+        elsif @current_currencys[target_currency] && @current_currencys[target_currency].key?(current_currency)
+            value = (value/@current_currencys[target_currency].values.first).round(2)
         end
     end
 end
+
+currency = Currency.new
+puts(currency.convert(1.0 , :BTC , :ARS))
+# puts currency.convert(1.0 , :ARS , :EUR)
+# ^^^^ Verificar
+# puts(currency.convert(1.0 , :USD , :BRL))
--- spec/currency_spec.rb
+++ spec/currency_spec.rb
@@ -4,7 +4,7 @@ require 'currency'
 describe 'Currency converter' do
     it 'Deve verificar se a moeda atual é igual a moeda objetivo'  do
         currency = Currency.new
-        expect(currency.convert(1.0 , :BRL , :BRL)).to eq(true)
+        expect(currency.convert(1.0 , :BRL , :BRL)).to eq(1.0)
     end
     it 'Caso tenha conversor direto , o realizar corretamente' do
         currency = Currency.new
@@ -16,6 +16,17 @@ describe 'Currency converter' do
 
     end
     it 'Caso nao tenha conversor direto , o relizar corretamente' do
-        
+        currency = Currency.new
+        expect(currency.convert(1.0 , :EUR , :GBP)).to eq(0.92)
+        expect(currency.convert(1.0 , :USD , :GBP)).to eq(0.78)
+        expect(currency.convert(1.0 , :EUR , :ARS)).to eq(95.24)
+        expect(currency.convert(1.0 , :EUR , :BRL)).to eq(6.67)
+        expect(currency.convert(1.0 , :USD , :ARS)).to eq(80.71)
+        expect(currency.convert(1.0 , :BTC , :ARS)).to eq(863642.86)
+        expect(currency.convert(1.0 , :ARS , :EUR)).to eq()
+        expect(currency.convert(1.0 , :EUR , :BTC)).to eq()
+        expect(currency.convert(1.0 , :BRL , :BTC)).to eq()
+        expect(currency.convert(1.0 , :BTC , :BRL)).to eq()
+
     end
 end
